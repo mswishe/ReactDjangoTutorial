c078b2c1675ed5a122850141fbf3b7f0a0f12146
Molly Swisher <mswisher1223@gmail.com>
2023-10-13 12:36:49 -0400

    added ui and functionality to home page

--- WebApp/music_controller/frontend/src/components/HomePage.js
+++ WebApp/music_controller/frontend/src/components/HomePage.js
@@ -2,18 +2,56 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
-import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null
+        };
+    }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    roomCode: data.code
+                });
+            });
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" component="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={ Link }>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={ Link }>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
     }
 
     render() {
         return (
             <Router>
                 <Routes>
-                    <Route path="/" element={<p>This is the home page</p>}></Route>
+                    <Route path="/" element={this.state.roomCode ? 
+                        <Navigate to={`/room/${this.state.roomCode}`} replace/> : 
+                        this.renderHomePage()}></Route>
                     <Route path="/join" element={<RoomJoinPage />} />
                     <Route path="/create" element={<CreateRoomPage />} />
                     <Route path="/room/:roomCode" element={<Room />} />
